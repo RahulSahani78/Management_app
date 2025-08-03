08385693d53a66073e971918dfcfed72646668f5
RahulSahani78 <rahulsahani7887@gmail.com>
2025-08-03 11:27:10 +0530

    Add app.js

--- api/app.js
+++ api/app.js
@@ -1,42 +1,37 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const cors=require('cors');
-const bodyParser=require('body-parser');
-const userRoute=require('./routes/user');
-const courseRoute=require('./routes/course');
-const studentRoute=require('./routes/student');
-const feeRoute=require('./routes/fee');
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
+// Routes
+const userRoute = require('./routes/user');
+const courseRoute = require('./routes/course');
+const studentRoute = require('./routes/student');
+const feeRoute = require('./routes/fee');
 
-mongoose.connect('mongodb+srv://sm123:1234@cluster0.tsful.mongodb.net/?retryWrites=true&w=majority&appName=sm')
-.then(()=>{
-   console.log("connected db"); 
-})
-.catch(err=>{
-  console.log(err)  ;
-})
-app.use(fileUpload({
-    useTempFiles : true
-    // tempFileDir : '/tmp/'
-}));
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("connected db"))
+  .catch(err => console.log(err));
 
+// Middleware
+app.use(fileUpload({ useTempFiles: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/user',userRoute);
-app.use('/course',courseRoute);
-app.use('/student',studentRoute);
-app.use('/fee',feeRoute);
 
-app.use('*',(req,res)=>{
-res.status(404).json({
-  msg:'bad request'  
+// Routes
+app.use('/user', userRoute);
+app.use('/course', courseRoute);
+app.use('/student', studentRoute);
+app.use('/fee', feeRoute);
+
+// 404 Handler
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'bad request' });
 });
 
-});
-
-
-
-
-module.exports=app;
+module.exports = app;
